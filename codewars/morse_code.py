595b4db3655d78c07ab094a1b688176acae6c8af
Scott <jshensley@icloud.com>
2023-12-25 18:23:11 -0500

    bowling

--- codewars/morse_code.py
+++ codewars/morse_code.py
@@ -1,4 +1,4 @@
-def decode_morse(morse_code: str) -> str:
+def decode_morse( morse_code: str ) -> str:
     """
     Decodes a string of Morse code into human-readable text.
 
@@ -20,25 +20,51 @@ def decode_morse(morse_code: str) -> str:
     'ALIVE'
     """
     return ("".join(
-            [MORSE_CODE[i] if i != "" else " " for i in morse_code.split(" ")])
-            .replace("  ", " ").strip())
+            [ MORSE_CODE[ i ] if i != "" else " " for i in morse_code.split( " " ) ] )
+            .replace( "  ", " " ).strip())
 
 
+def decode_bits( bits ):
+    # ToDo: Accept 0's and 1's, return dots, dashes and spaces
+    return bits.replace( '111', '-' ).replace( '000', ' ' ).replace( '1', '.' ).replace( '0', '' )
 
 
+def decode_morse( morseCode ):
+    # ToDo: Accept dots, dashes and spaces, return human-readable message
+    return morseCode.replace( '.', MORSE_CODE[ '.' ] ).replace( '-', MORSE_CODE[ '-' ] ).replace( ' ', '' )
 
-MORSE_CODE = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z', '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9', '.-.-.-': '.', '--..--': ',', '..--..': '?', '.----.': "'", '-.-.--': '!', '-..-.': '/', '-.--.': '(', '-.--.-': ')', '.-...': '&', '---...': ':', '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-', '..--.-': '_', '.-..-.': '"', '...-..-': '$', '.--.-.': '@', '...---...': 'SOS'}
 
-a = decode_morse('.-') # , 'A')
-b = decode_morse('--...') # , '7')
-c = decode_morse('...-..-') # , '$')
-d = decode_morse('.') # , 'E')
-e = decode_morse('..') # , 'I')
-f = decode_morse('. .') #, 'EE')
-g = decode_morse('.   .') # , 'E E')
-h = decode_morse('...-..- ...-..- ...-..-') # , '$$$')
-i = decode_morse('----- .---- ..--- ---.. ----.') # , '01289')
-j = decode_morse('.-... ---...   -..-. --...') # , '&: /7')
-k = decode_morse('...---...') # , 'SOS')
-l = decode_morse('... --- ...') # , 'SOS')
-m = decode_morse('...   ---   ...') # , 'S O S')
+MORSE_CODE = {
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I',
+    '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R',
+    '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z', '-----': '0',
+    '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8',
+    '----.': '9', '.-.-.-': '.', '--..--': ',', '..--..': '?', '.----.': "'", '-.-.--': '!', '-..-.': '/', '-.--.': '(',
+    '-.--.-': ')', '.-...': '&', '---...': ':', '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-', '..--.-': '_',
+    '.-..-.': '"', '...-..-': '$', '.--.-.': '@', '...---...': 'SOS' }
+
+# a = decode_morse( '.-' )  # , 'A')
+# b = decode_morse( '--...' )  # , '7')
+# c = decode_morse( '...-..-' )  # , '$')
+# d = decode_morse( '.' )  # , 'E')
+# e = decode_morse( '..' )  # , 'I')
+# f = decode_morse( '. .' )  # , 'EE')
+# g = decode_morse( '.   .' )  # , 'E E')
+# h = decode_morse( '...-..- ...-..- ...-..-' )  # , '$$$')
+# i = decode_morse( '----- .---- ..--- ---.. ----.' )  # , '01289')
+# j = decode_morse( '.-... ---...   -..-. --...' )  # , '&: /7')
+# k = decode_morse( '...---...' )  # , 'SOS')
+# l = decode_morse( '... --- ...' )  # , 'SOS')
+# m = decode_morse( '...   ---   ...' )  # , 'S O S')
+
+"""
+When transmitting the Morse code, the international standard specifies that:
+
+"Dot" – is 1 time unit long.
+"Dash" – is 3 time units long.
+Pause between dots and dashes in a character – is 1 time unit long.
+Pause between characters inside a word – is 3 time units long.
+Pause between words – is 7 time units long.
+"""
+
+aa = decode_morse(decode_bits('1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011')) #, 'HEY JUDE')
--- codewars/smallest_possible_sum.py
+++ codewars/smallest_possible_sum.py
@@ -1,40 +1,17 @@
 from math import gcd
+from typing import List
 
-
-def solution(a: list) -> int:
+def solution(a: List[int]) -> int:
     """
-    Apply this algorithm, if X[i] > X[j] then X[i] = X[i] - X[j], recursively until it can not be done any more.
-
-    The list [6, 9, 21] iterates as:
-        X_1 = [6, 9, 12] # -> X_1[2] = X_1[2] - X_1[1] = 21 - 9
-        X_2 = [6, 9, 6]  # -> X_2[2] = X_1[2] - X_1[0] = 12 - 6
-        X_3 = [6, 3, 6]  # -> X_3[1] = X_2[1] - X_2[0] = 9 - 6
-        X_4 = [6, 3, 3]  # -> X_4[2] = X_3[2] - X_3[1] = 6 - 3
-        X_5 = [3, 3, 3]  # -> X_5[1] = X_4[0] - X_4[1] = 6 - 3
-
-        [6, 9, 12] #-> X[2] = 21 - 9
-        [6, 9, 6] #-> X[2] = 12 - 6
-        [6, 3, 6] #-> X[1] = 9 - 6
-        [6, 3, 3] #-> X[2] = 6 - 3
-        [3, 3, 3] #-> X[1] = 6 - 3
-
-    There are no further iterations possible, so the algorithm stops and the result is [3, 3, 3].
+    Calculate the sum of an array after repeatedly applying the operation:
+    if X[i] > X[j] then X[i] = X[i] - X[j] until all elements are equal.
+    The result is the greatest common divisor (GCD) of all elements multiplied by the length of the array.
 
     Args:
-        a (): list of integers
+        a (List[int]): A list of integers to transform.
 
-    Returns: the sum of the remaining numbers
-
+    Returns:
+        int: The sum of the elements after transformation.
     """
-    let = "b"
-    if len(a) == 1:
-        return a[0]
-    elif len(a) == 2:
-        return math.gcd(a[0], a[1]) + math.gcd(a[0], a[1])
-    elif len(a) >= 3:
-        return gcd(*a) * len(a)
-
-
-a = solution([9])  # , 9)
-b = solution([6, 9, 21])  # , 9)
-c = solution([1, 21, 55])  # , 3)
+    # Calculate the GCD for all elements and multiply by length of the array.
+    return gcd(*a) * len(a)
