cd40b095db3fd3c0375214001cc8639cb4946b84
Scott <jshensley@icloud.com>
2023-12-30 14:20:27 -0500

    rectangle rotation

---
+++ codewars/rectangle_rotation.py
@@ -0,0 +1,13 @@
+def rectangle_rotation(a, b):
+    pass
+
+
+a = rectangle_rotation(6, 4)  # 23)
+b = rectangle_rotation(30, 2)  # 65)
+c = rectangle_rotation(8, 6)  # 49)
+d = rectangle_rotation(16, 20)  # 333)
+
+print(6 / 4)
+print(30 / 2)
+print(8 / 6)
+print(16 / 20)
