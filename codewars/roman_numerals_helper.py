c18cb248aadb94aad1240a5807cfc5694e1a8a3d
jshensley <seizure_recognition@icloud.com>
2024-04-14 08:42:01 -0400

    fixing from rom

--- codewars/roman_numerals_helper.py
+++ codewars/roman_numerals_helper.py
@@ -10,6 +10,9 @@ Input range : 1 <= n < 4000
 In this kata 4 should be represented as IV, NOT as IIII (the "watchmaker's four").
 """
 
+# import queue
+import collections
+print(help(collections.deque))
 
 class RomanNumerals:
 	ROMAN_NUMERALS = {
@@ -41,25 +44,51 @@ class RomanNumerals:
 		return digits
 
 	@staticmethod
-	def from_roman(roman_num: str) -> int:
-		return sum(map(lambda x: RomanNumerals.ROMAN_NUMERALS[x], [i for i in roman_num]))
-
-	@staticmethod
 	def to_roman(val: int) -> str:
 		return ''.join([RomanNumerals.NUMERAL_ROMANS[i] for i in RomanNumerals.break_into_base10(val)])
+
+	@staticmethod
+	def from_roman(roman_num: str) -> int:
+		print(f'Roman Num: {roman_num}')
+		lis = [i for i in roman_num]
+		print(f'List: {lis}')
+		print(RomanNumerals.ROMAN_NUMERALS)
+		output = 0
+		test_string = ''
+		for idx, let in enumerate(lis):
+			test_string += let
+			try:
+				test_string += lis[idx + 1]
+			except IndexError:
+				pass
+			if test_string in RomanNumerals.ROMAN_NUMERALS:
+				output += RomanNumerals.ROMAN_NUMERALS[test_string]
+				test_string = ''
+			else:
+				keep_next_value = [i for i in test_string]
+				for i in keep_next_value:
+					if i in RomanNumerals.ROMAN_NUMERALS:
+						output += RomanNumerals.ROMAN_NUMERALS[i]
+						test_string = ''
+						test_string = i
+						continue
+
+			# output += sum(map(lambda x: RomanNumerals.ROMAN_NUMERALS[x], keep_next_value))
+		return output
 
 
 a = RomanNumerals()
 # To Roman
-print(a.ROMAN_NUMERALS, a.NUMERAL_ROMANS, sep='\n\n')
-print(a.to_roman(1000))  # 'M'
-print(a.to_roman(2000))  # 'MM''
-print(a.to_roman(1666))  # 'MDCLXVI'
-print(a.to_roman(86))  # 'LXXXVI'
-print(a.to_roman(1))  # 'I'
+# print(a.ROMAN_NUMERALS, a.NUMERAL_ROMANS, sep='\n\n')
+# print(a.to_roman(1000))  # 'M'
+# print(a.to_roman(2000))  # 'MM''
+# print(a.to_roman(1666))  # 'MDCLXVI'
+# print(a.to_roman(86))  # 'LXXXVI'
+# print(a.to_roman(1))  # 'I'
 
 # From Roman
 # print(a.from_roman("MM"))  # 2000
 # print(a.from_roman("MDCLXVI"))  # 1666
 # print(a.from_roman("LXXXVI"))  # 86
 # print(a.from_roman("I"))  # 1
+print(a.from_roman("DCLIX"))  # 659
