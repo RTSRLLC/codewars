bc05e9989c92f146c8611b75239f10898fa777e4
jshensley <seizure_recognition@icloud.com>
2024-05-05 13:16:32 -0400

    almost there

--- codewars/Human_readable_duration_format.py
+++ codewars/Human_readable_duration_format.py
@@ -1,45 +1,56 @@
 def format_duration(seconds):
-	def length_nums(alist, length):
-		years = str(nums[0]) + " year" if nums[0] == 1 else str(nums[0]) + " years"
-		days = str(nums[1]) + " day" if nums[1] == 1 else str(nums[1]) + " days"
-		hours = str(nums[2]) + " hour" if nums[2] == 1 else str(nums[2]) + " hours"
-		minutes = str(nums[3]) + " minute" if nums[3] == 1 else str(nums[3]) + " minutes"
-		second = str(nums[4]) + " second" if nums[4] == 1 else str(nums[4]) + " seconds"
 	
 	the_num = 0
 	nums = []
 	if seconds == 0:
 		return "now"
-	if seconds >= secs_in_year:
+	if seconds == 1:
+		return "1 second"
+	if seconds >= 31536000:
 		the_num = seconds // 31536000
-		nums.append(the_num)
 		seconds -= the_num * 31536000
-	if seconds >= secs_in_day:
+		if seconds == 0:
+			nums.append(str(the_num) + " year" if the_num == 1 else str(the_num) + " years")
+			return " and ".join(i for i in nums)
+		nums.append(str(the_num) + " year, " if the_num == 1 else str(the_num) + " years, ")
+	if seconds >= 86400:
 		the_num = seconds // 86400
-		nums.append(the_num)
 		seconds -= the_num * 86400
-	if seconds >= secs_in_hour:
+		if seconds == 0:
+			nums.append(str(the_num) + " day" if the_num == 1 else str(the_num) + " days")
+			return " and ".join(i for i in nums)
+		nums.append(str(the_num) + " day, " if the_num == 1 else str(the_num) + " days, ")
+	if seconds >= 3600:
 		the_num = seconds // 3600
-		nums.append(the_num)
 		seconds -= the_num * 3600
-	if seconds >= secs_in_min:
+		if seconds == 0:
+			nums.append(str(the_num) + " hour" if the_num == 1 else str(the_num) + " hours")
+			return " and ".join(i for i in nums)
+		nums.append(str(the_num) + " hour ," if the_num == 1 else str(the_num) + " hours, ")
+	if seconds >= 60:
 		the_num = seconds // 60
-		nums.append(the_num)
 		seconds -= the_num * 60
-	if seconds > 0:
-		nums.append(seconds)
-	
-	return ", ".join([years, days, hours, minutes, second])
+		if seconds == 0:
+			nums.append(" and " + str(the_num) + " minute" if the_num == 1 else " and " + str(the_num) + " minutes")
+			return " ".join(i for i in nums)
+		nums.append(str(the_num) + " minute, " if the_num == 1 else str(the_num) + " minutes, ")
+	if 1 <= seconds < 60:
+		if seconds == 1:
+			nums.append(" and " + str(seconds) + " second")
+			return "".join(i for i in nums)
+		the_num = seconds
+		nums.append(" and " + str(the_num) + " second" if the_num == 1 else " and " + str(the_num) + " seconds")
+		return "".join(i for i in nums)
 
 
-# a = format_duration(0)  # , "now")
-# b = format_duration(1)  # , "1 second")
-# c = format_duration(62)  # , "1 minute and 2 seconds")
-# d = format_duration(120)  # , "2 minutes")
-# e = format_duration(3600)  # , "1 hour")
-# f = format_duration(3662)  # , "1 hour, 1 minute and 2 seconds")
-# g = format_duration(15731080)  # , "182 days, 1 hour, 44 minutes and 40 seconds")
-# h = format_duration(132030240)  # , "4 years, 68 days, 3 hours and 4 minutes")
-# i = format_duration(205851834)  # , "6 years, 192 days, 13 hours, 3 minutes and 54 seconds")
+a = format_duration(0)  # , "now")
+b = format_duration(1)  # , "1 second")
+c = format_duration(62)  # , "1 minute and 2 seconds")
+d = format_duration(120)  # , "2 minutes")
+e = format_duration(3600)  # , "1 hour")
+f = format_duration(3662)  # , "1 hour, 1 minute and 2 seconds")
+g = format_duration(15731080)  # , "182 days, 1 hour, 44 minutes and 40 seconds")
+h = format_duration(132030240)  # , "4 years, 68 days, 3 hours and 4 minutes")
+i = format_duration(205851834)  # , "6 years, 192 days, 13 hours, 3 minutes and 54 seconds")
 j = format_duration(253374061)  # , "8 years, 12 days, 13 hours, 41 minutes and 1 second")
 k = format_duration(242062374)  # , "7 years, 246 days, 15 hours, 32 minutes and 54 seconds")
