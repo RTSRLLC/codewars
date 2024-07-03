ac7d076660798a9cf5a60eb40485f6d65efa3674
jshensley <seizure_recognition@icloud.com>
2024-07-03 12:41:39 -0400

    initial conditions being met

--- codewars/is_interesting.py
+++ codewars/is_interesting.py
@@ -10,41 +10,30 @@ def is_interesting(number, awesome_phrases):
 	if all(list(str(number))) == str(list(str(number))[0]):
 		return 2
 	# The digits are sequential, incrementing†: 1234
-	try:
-		if number == int(''.join([str(i) for i in range(int(str(number)[0]), len(str(number)) + 1)])):
-			return 2
-	except ValueError:  # handle the case where the number is 7890 0 should come after 9, and not before  1, as in 7890.
-		pass
+	a = str(number)
+	if str(number) in '1234567890':
+		return 2
 	# The digits are sequential, decrementing‡: 4321
-	try:
-		if number == int(''.join([str(i) for i in range(a, 0, -1)])):
-			return 2
-	except ValueError:  # handle the case where the number is 3210 0 should come after 1, and not before  9, as in 3210.
-		pass
+	# if number == int(''.join([str(i) for i in range(a, 0, -1)])):
+	# 	return 2
 
 
-a = is_interesting(3, [1337, 256])  # 0
-b = is_interesting(1336, [1337, 256])  # 1
-c = is_interesting(1337, [1337, 256])  # 2
-d = is_interesting(11208, [1337, 256])  # 0
-e = is_interesting(11209, [1337, 256])  # 1
-f = is_interesting(11211, [1337, 256])  # 2
-g = is_interesting(1111, [1337, 256])  # 2
-# 1234
+# a = is_interesting(3, [1337, 256])  # 0
+# b = is_interesting(1336, [1337, 256])  # 1
+# c = is_interesting(1337, [1337, 256])  # 2
+# d = is_interesting(11208, [1337, 256])  # 0
+# e = is_interesting(11209, [1337, 256])  # 1
+# f = is_interesting(11211, [1337, 256])  # 2
+# g = is_interesting(1111, [1337, 256])  # 2
 h = is_interesting(1234, [1337, 256])  # 2ih = is_interesting(7890, [1337, 256])  # 2
-# 7890
 i = is_interesting(7890, [1337, 256])  # 2
-# 4321
 j = is_interesting(4321, [1337, 256])  # 2
 
 # 2 if the number is "interesting"
 # 1 if an interesting number occurs within the next two miles
 # 0 if the number is not interesting
-
 # "Interesting" Numbers
-#
 # Interesting numbers are 3-or-more digit numbers that meet one or more of the following criteria:
-#
 # Any digit followed by all zeros: 100, 90000
 # Every digit is the same number: 1111
 # The digits are sequential, incementing†: 1234
