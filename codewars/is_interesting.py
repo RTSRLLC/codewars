5f997810c3aafe435f35df0b111efe2556ee6500
jshensley <seizure_recognition@icloud.com>
2024-07-04 06:58:25 -0400

    need to focus on awesome phrases

--- codewars/is_interesting.py
+++ codewars/is_interesting.py
@@ -1,31 +1,46 @@
 def is_interesting(number, awesome_phrases):
-	if len(str(number)) < 3:
+	a, b, c = number, number + 1, number + 2
+	
+	if (len(str(a)) < 3 or
+			len(str(b)) < 3 or
+			len(str(c)) < 3):
 		return 0
-	if number in awesome_phrases:
+	if a in awesome_phrases:
 		return 2
-	if number % 100 == 0:
+	if (a % 100 == 0 or
+			b % 100 == 0 or
+			c % 100 == 0):
+		return 2 if number % 100 == 0 else 1
+	if (str(a) == str(a)[::-1] or
+			str(b) == str(b)[::-1] or
+			str(c) == str(c)[::-1]):
+		return 2 if str(a) == str(a)[::-1] else 1
+	if (all(list(str(a))) == str(list(str(a))[0]) or
+			all(list(str(b))) == str(list(str(b))[0]) or
+			all(list(str(c))) == str(list(str(c))[0])):
+		return 2 if all(list(str(a))) == str(list(str(a))[0]) else 1
+	if ((str(a) in '1234567890' or str(a) in '9876543210') or
+			(str(b) in '1234567890' or str(b) in '9876543210') or
+			(str(c) in '1234567890' or str(c) in '9876543210')):
 		return 2
-	if str(number) == str(number)[::-1]:
-		return 2
-	if all(list(str(number))) == str(list(str(number))[0]):
-		return 2
-	a = str(number)
-	if str(number) in '1234567890':
-		return 2
-	if str(number) in '0987654321':
-		return 2
+	return 0
 
 
-a = is_interesting(3, [1337, 256])  # 0
-b = is_interesting(1336, [1337, 256])  # 1
+a = is_interesting(97, [1337, 256])  # 0
+
+b = is_interesting(1336, [1337, 256])  # 1  still off haven't dealt with awesome_phrases + 1, + 2
+
 c = is_interesting(1337, [1337, 256])  # 2
 d = is_interesting(11208, [1337, 256])  # 0
 e = is_interesting(11209, [1337, 256])  # 1
 f = is_interesting(11211, [1337, 256])  # 2
 g = is_interesting(1111, [1337, 256])  # 2
-h = is_interesting(1234, [1337, 256])  # 2ih = is_interesting(7890, [1337, 256])  # 2
+h = is_interesting(1234, [1337, 256])  # 2
 i = is_interesting(7890, [1337, 256])  # 2
 j = is_interesting(4321, [1337, 256])  # 2
+k = is_interesting(300, [1337, 256])  # 2
+ll = is_interesting(298, [1337, 256])  # 1
+m = is_interesting(299, [1337, 256])  # 1
 
 # 2 if the number is "interesting"
 # 1 if an interesting number occurs within the next two miles
