80a30147fc17e54b3cd0b2040054407130c05c96
jshensley <seizure_recognition@icloud.com>
2024-07-08 07:05:48 -0400

    it passed but it's ugly

--- codewars/is_interesting_car_milegae.py
+++ codewars/is_interesting_car_milegae.py
@@ -1,49 +1,40 @@
 def is_interesting(number, awesome_phrases):
 	a, b, c = number, number + 1, number + 2
-	awesome = awesome_phrases
-	print(f"a, b, c: {a, b, c}   awesome_phrases: {awesome_phrases}")
-	
-	if a > 1_000_000_000 and b > 1_000_000_000 and c > 1_000_000_000:
-		print(0)
+	if a >= 1_000_000_000 and b >= 1_000_000_000 and c >= 1_000_000_000:
 		return 0
-	
-	elif len(str(a)) < 3 and len(str(b)) < 3 and len(str(c)) < 3:
+	if len(str(a)) < 3 and len(str(b)) < 3 and len(str(c)) < 3:
 		return 0
-	
-	elif a in awesome_phrases:
-		return 2 if a in awesome_phrases else 1
+	if a in awesome_phrases:
+		return 2
 	elif b in awesome_phrases:
-		return 2 if b in awesome_phrases else 1
+		return 1
 	elif c in awesome_phrases:
-		return 2 if c in awesome_phrases else 1
-	
-	elif (a % 100 == 0 or
-	      b % 100 == 0 or
-	      c % 100 == 0):
-		print(a % 100, b % 100, c % 100)
-		print(2 if (a and b and c) % 100 == 0 else 1)
-		return 2 if (a % 100 == 0 and b % 100 == 0 and c % 100 == 0) else 1
-	
-	elif (all(list(str(a))) == str(list(str(a))[0]) or
-	      all(list(str(b))) == str(list(str(b))[0]) or
-	      all(list(str(c))) == str(list(str(c))[0])):
-		print(2 if all(list(str(a))) == str(list(str(a))[0]) else 1)
-		return 2 if all(list(str(a))) == str(list(str(a))[0]) else 1
-	
-	elif ((str(a) in '1234567890' or str(a) in '9876543210') or
-	      (str(b) in '1234567890' or str(b) in '9876543210') or
-	      (str(c) in '1234567890' or str(c) in '9876543210')):
-		print(2 if str(a) in '1234567890' or str(a) in '9876543210' else 1)
-		return 2 if str(a) in '1234567890' or str(a) in '9876543210' else 1
-	
-	elif (str(a) == str(a)[::-1] or
-	      str(b) == str(b)[::-1] or
-	      s
-		print(2 if str(a) == str(a)[::-1] else 1)
-		return 2 if str(a) == str(a)[::-1] else 1
+		return 1
+	if a % 100 == 0 or (100 <= a <= 1_000_000_000) and str(a) == str(a)[::-1]:
+		return 2
+	elif b % 100 == 0:
+		return 1
+	elif c % 100 == 0:
+		return 1
 	else:
-		return 0
+		pass
+	if (all(list(str(a))) == str(list(str(a))[0]) or
+			all(list(str(b))) == str(list(str(b))[0]) or
+			all(list(str(c))) == str(list(str(c))[0])):
+		return 2 if all(list(str(a))) == str(list(str(a))[0]) else 1
+	if ((str(a) in '1234567890' or str(a) in '9876543210') or
+			(str(b) in '1234567890' or str(b) in '9876543210') or
+			(str(c) in '1234567890' or str(c) in '9876543210')):
+		return 2 if str(a) in '1234567890' or str(a) in '9876543210' else 1
+	if (str(a) == str(a)[::-1] or
+			str(b) == str(b)[::-1] or
+			str(c) == str(c)[::-1]):
+		return 2 if str(a) == str(a)[::-1] else 1
+	return 0
 
+
+# and Operator: If the left operand is false, Python does not evaluate the right operand
+# or Operator: If the left operand is true, Python does not evaluate the right operand
 
 a = is_interesting(98, [1337, 256])  # 1
 aa = is_interesting(99, [1337, 256])  # 1
