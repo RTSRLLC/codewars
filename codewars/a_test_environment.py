97c27c419f62aa4c54271eeabfad38b3a4c838e5
jshensley <seizure_recognition@icloud.com>
2024-08-03 08:42:43 -0400

    getting closer neee the opposite diagonal

--- codewars/a_test_environment.py
+++ codewars/a_test_environment.py
@@ -14,41 +14,53 @@ def who_is_winner(pieces_position_list):
 		pd_board.at[col_dict[color[0]][0], color[0]] = move, color
 		col_dict[color[0]][0] -= 1
 		col_dict[color[0]][1] += 1
-	# if move >= 7:
-	# ================================
-	# test various sorting methods on pieces_position_list
-	# could be good for column checks
-	on_x0 = sorted(pieces_position_list, key=lambda x: x[0])
-	count_x0 = Counter(on_x0)
-	greater_then_4 = [k[0] for k, v in count_x0.items() if v >= 4]
-	# get items in board_array_transposed that are in greater_then_4
-	pd_board.columns = list(col_dict.keys())
-	possible_winning_rows = [[row, [i for i in col if i is not None]] for row, col in pd_board.T.iterrows() if row in greater_then_4]
-	print("possible_winning_rows")
-	# the idea here is to identify the player adn their color that achieved four in a row first based on their number of moves
-	for item in possible_winning_rows:
-		count = 1
-		competitor = item[1][0][1][2:]
-		for i in item[1][1:]:
-			print(i)
-			if i[1][2:] == competitor:
-				count += 1
-				if count == 4:
-					return competitor
-			elif i[1][2:] != competitor:
+		# winning move is: {'the_row_series': [(1, 'B_Red'), (7, 'C_Red'), (3, 'D_Red'), (5, 'E_Red'), (2, 'G_Yellow')]}
+		if move >= 7:
+			the_col_series = pd_board[color[0]].dropna().tolist()
+			if len(the_col_series) >= 4:
+				col_list_for_win = [i[1][2:] for i in the_col_series]
 				count = 0
-				competitor = i[1][2:]
+				for i in col_list_for_win:
+					if i == color[2:]:
+						count += 1
+					else:
+						count = 0
+					if count == 4:
+						return color[2:]
+			the_row_series = pd_board[pd_board.apply(lambda row: row[color[0]] == (move, color), axis=1)].iloc[0].dropna().tolist()
+			if len(the_row_series) >= 4:
+				row_list_for_win = [i[1][2:] for i in the_row_series]
+				count = 0
+				for i in row_list_for_win:
+					if i == color[2:]:
+						count += 1
+					else:
+						count = 0
+					if count == 4:
+						return color[2:]
+			for i in range(-5, 5):
+				the_diagonal_series = pd.Series(pd_board.to_numpy().diagonal(offset=i)).dropna().tolist()
+				if (move, color) in the_diagonal_series and len(the_diagonal_series) >= 4:
+					diagonal_list_for_win = [i[1][2:] for i in the_diagonal_series]
+					count = 0
+					for i in diagonal_list_for_win:
+						if i == color[2:]:
+							count += 1
+						else:
+							count = 0
+						if count == 4:
+							return color[2:]
 	
 	return "Draw"
 
 
-# a = who_is_winner(
-# 	[
-# 		"C_Yellow", "E_Red", "G_Yellow", "B_Red", "D_Yellow", "B_Red", "B_Yellow", "G_Red", "C_Yellow", "C_Red",
-# 		"D_Yellow", "F_Red", "E_Yellow", "A_Red", "A_Yellow", "G_Red", "A_Yellow", "F_Red", "F_Yellow", "D_Red",
-# 		"B_Yellow", "E_Red", "D_Yellow", "A_Red", "G_Yellow", "D_Red", "D_Yellow", "C_Red"
-# 		]
-# 	)  # , "Yellow"))
+a = who_is_winner(
+	[
+		"C_Yellow", "E_Red", "G_Yellow", "B_Red", "D_Yellow", "B_Red", "B_Yellow", "G_Red", "C_Yellow", "C_Red",
+		"D_Yellow", "F_Red", "E_Yellow", "A_Red", "A_Yellow", "G_Red", "A_Yellow", "F_Red", "F_Yellow", "D_Red",
+		"B_Yellow", "E_Red", "D_Yellow", "A_Red", "G_Yellow", "D_Red", "D_Yellow", "C_Red"
+		]
+	)  # , "Yellow"))
 #
 # b = who_is_winner(
 # 	[
@@ -89,12 +101,12 @@ def who_is_winner(pieces_position_list):
 # 		]
 # 	)  # , "Draw")
 #
-g = who_is_winner(
-	[
-		'B_Red', 'G_Yellow', 'D_Red', 'G_Yellow', 'E_Red', 'G_Yellow', 'C_Red', 'D_Yellow', 'B_Red', 'G_Yellow',
-		'A_Red', 'A_Yellow', 'B_Red', 'C_Yellow', 'A_Red', 'B_Yellow', 'B_Red', 'D_Yellow', 'D_Red', 'A_Yellow',
-		'E_Red', 'G_Yellow', 'G_Red']
-	)  # , "Red")
+# g = who_is_winner(
+# 	[
+# 		'B_Red', 'G_Yellow', 'D_Red', 'G_Yellow', 'E_Red', 'G_Yellow', 'C_Red', 'D_Yellow', 'B_Red', 'G_Yellow',
+# 		'A_Red', 'A_Yellow', 'B_Red', 'C_Yellow', 'A_Red', 'B_Yellow', 'B_Red', 'D_Yellow', 'D_Red', 'A_Yellow',
+# 		'E_Red', 'G_Yellow', 'G_Red']
+# 	)  # , "Red")
 #
 # h = who_is_winner(
 # 	['B_Red', 'A_Yellow', 'C_Red', 'E_Yellow', 'A_Red', 'C_Yellow', 'E_Red', 'A_Yellow', 'D_Red', 'C_Yellow', 'C_Red',
