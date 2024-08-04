e180c0a7c525b748813a31b6a55757de51720c3f
jshensley <seizure_recognition@icloud.com>
2024-08-04 12:20:48 -0400

    got it working need refactoring.

--- codewars/a_test_environment.py
+++ codewars/a_test_environment.py
@@ -26,42 +26,50 @@ def diagonal(df, mv=None, colr=None):
 
 
 def who_is_winner(pieces_position_list: list) -> str:
-	print(pieces_position_list, len(pieces_position_list), sep='\n', end='\n')
 	# provide a countdown for each column in order to place the pieces for user visualization
 	col_dict = {"A": [5, 0], "B": [5, 0], "C": [5, 0], "D": [5, 0], "E": [5, 0], "F": [5, 0], "G": [5, 0]}
+	
 	# for user board visualization and ensure all cells are tuples for later code
+	# create numpy array and convert to pandas DataFrame amking the numbers string tuples.
 	board = np.ones((6, 7), dtype=object) * 10
 	pd_board = pd.DataFrame(board, columns=list('ABCDEFG')).applymap(lambda x: tuple(str(x)))
+	
 	# Place each piece on the board
 	for move, color in list(enumerate(pieces_position_list, start=1)):
 		# a tuple with move is needed to ensure the number of moves is not lost
-		pd_board.at[col_dict[color[0]][0], color[0]] = move, color  # a tuple to match the board
+		pd_board.at[col_dict[color[0]][0], color[0]] = move, color  # a tuple to match the existing board
 		col_dict[color[0]][0] -= 1
 		col_dict[color[0]][1] += 1
+		
 		# check for winning conditions if there are at least 7 moves, the min for a win
 		if move >= 7:
-			# check columns for a win
+			# check current column for a win
 			the_col_series = pd_board[color[0]].tolist()
-			if len(the_col_series) >= 4:
-				col_list_for_win = [i[1][2:] for i in the_col_series]
-				col_count = the_counting_list(col_list_for_win, color[2:])
+			if len(the_col_series) >= 4:  # at least 4 needed to win
+				col_list_for_win = [i[1][2:] for i in the_col_series]  # put the col elements in a list
+				col_count = the_counting_list(col_list_for_win, color[2:])  # check for a win or None if not
 				if col_count is not None:
 					return col_count
-			# check rows for a win
-			the_row_series = pd_board[pd_board.apply(lambda row: row[color[0]] == (move, color), axis=1)].iloc[
-				0].tolist()
-			if len(the_row_series) >= 4:
-				row_list_for_win = [i[1][2:] for i in the_row_series]
-				row_count = the_counting_list(row_list_for_win, color[2:])
+				
+			# check current row for a win
+			the_row_series = (
+				pd_board[pd_board.apply(lambda row: row[color[0]] == (move, color), axis=1)].iloc[0].tolist())
+			if len(the_row_series) >= 4:  # at least 4 needed to win
+				row_list_for_win = [i[1][2:] for i in the_row_series]  # put the row elements in a list
+				row_count = the_counting_list(row_list_for_win, color[2:])  # check for a win or None if not
 				if row_count is not None:
 					return row_count
-			# check diagonals for a win
+			
+			# check negative (left to right top to bottom) diagonals for a win
 			reg = diagonal(pd_board, mv=move, colr=color)
 			if reg is not None:
 				return diagonal(pd_board, mv=move, colr=color)
+			
+			# check positive (right to left top to bottom) diagonals for a win but flipped it for np.diagonal() reqs
 			reg_flip = diagonal(pd_board.iloc[:, ::-1], mv=move, colr=color)
 			if reg_flip is not None:
 				return diagonal(pd_board.iloc[:, ::-1], mv=move, colr=color)
+			
 	return "Draw"
 
 
