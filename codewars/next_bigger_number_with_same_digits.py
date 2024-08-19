6d2104b3b35c54bd22b241b19638376a1a26680d
jshensley <seizure_recognition@icloud.com>
2024-08-19 17:10:56 -0400

    next larger numnber

--- codewars/next_bigger_number_with_same_digits.py
+++ codewars/next_bigger_number_with_same_digits.py
@@ -16,55 +16,33 @@ import time
 
 
 def next_bigger(n):
-	print(n)
-	_ = sorted(list(set(int(''.join(x)) for x in list(permutations(str(n))))))
 	try:
-		return _[_.index(n) + 1]
+		_ = [i for i in permutations(str(n))]
+		_ = sorted(int(''.join(x)) for x in _ if int(''.join(x)) > n)[0]
+		return _ if _ else -1
 	except IndexError:
-		return -1
-
-
-def next_bigger_(n):
-	perm = list(permutations(str(n)))
-	print(f"perm:\n{list(perm)}")
-	perms_ = [int(''.join(x)) for x in perm]
-	print(f"perms_:\n{perms_}")
-	set_perms = set(perms_)
-	print(f"set_perms:\n{set_perms}")
-	list_perms = list(set_perms)
-	print(f"list_perms:\n{list_perms}")
-	sorted_perms = sorted(list_perms)
-	print(f"sorted_perms:\n{sorted_perms}")
-	# loc = sorted_perms.index(n)
-	# print(f"loc:\n{loc}")
-	try:
-		return sorted_perms.pop(sorted_perms.index(n) + 1)
-		# return sorted_perms[sorted_perms.index(n) + 1]
-	except IndexError:
+		print(f"No larger number found for {n}")
 		return -1
 
 
 start = time.time()
 
 # a = next_bigger(21)  # ,   -1)
-# aa = next_bigger_(21)  # ,   -1)
-# print('*' * 72)
+print('*' * 72)
 
 b = next_bigger(513)  # ,  531)
-bb = next_bigger_(513)  # ,  531)
 print('*' * 72)
 
 c = next_bigger(2017)  # , 2071)
-cc = next_bigger_(2017)  # , 2071)
 print('*' * 72)
 
 d = next_bigger(414)  # ,  441)
-dd = next_bigger_(414)  # ,  441)
 print('*' * 72)
 
 e = next_bigger(144)  # ,  414)
-ee = next_bigger_(144)  # ,  414)
 print('*' * 72)
+
+f = next_bigger(1234567890)  # , 1234567908)
 
 end = time.time()
 print(f"Time: {(end - start) * 1000:.2f} ms")
