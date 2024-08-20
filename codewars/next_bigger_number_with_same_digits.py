b874d77aa55cd23cb49618afa541b8fb58948ba2
jshensley <seizure_recognition@icloud.com>
2024-08-20 17:18:00 -0400

    next larger numnber

--- codewars/next_bigger_number_with_same_digits.py
+++ codewars/next_bigger_number_with_same_digits.py
@@ -16,19 +16,39 @@ import time
 
 
 def next_bigger(n):
-	try:
-		return int(''.join(sorted(i for i in permutations(str(n)) if int(''.join(i)) > n)[0]))
-	except IndexError:
-		return -1
+    digits = list(str(n))
+    length = len(digits)
+    
+    # Step 1: Find the rightmost digit that is smaller than the digit next to it
+    a = list(range(length - 2, -1, -1))
+    for i in range(length - 2, -1, -1):
+        if digits[i] < digits[i + 1]:
+            break
+    else:
+        return -1
+    
+    # Step 2: Find the smallest digit on the right side of the found digit that is larger than it
+    b = list(range(length - 1, i, -1))
+    for j in range(length - 1, i, -1):
+        if digits[j] > digits[i]:
+            break
+    
+    # Step 3: Swap the found digits
+    digits[i], digits[j] = digits[j], digits[i]
+    
+    # Step 4: Reverse the digits after the position i
+    digits = digits[:i + 1] + sorted(digits[i + 1:])
+    
+    return int(''.join(digits))
 
 
 start = time.time()
 
-a = next_bigger(21)  # ,   -1)
-print('*' * 72)
-
-b = next_bigger(513)  # ,  531)
-print('*' * 72)
+# a = next_bigger(21)  # ,   -1)
+# # print('*' * 72)
+# #
+# b = next_bigger(513)  # ,  531)
+# print('*' * 72)
 
 c = next_bigger(2017)  # , 2071)
 print('*' * 72)
