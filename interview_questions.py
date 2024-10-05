9008df28f96aac7de8a196c0d762965e258b41aa
jshensley <seizure_recognition@icloud.com>
2024-10-05 12:40:39 -0400

    quick sort

--- interview_questions.py
+++ interview_questions.py
@@ -1,180 +1,182 @@
 import random
+import math
 import array
 from collections import deque
 
 
 class Stack:
-	"""
-	A class used to represent a Stack data structure.
+    """
+    A class used to represent a Stack data structure.
 
-	Attributes
-	----------
-	stack : list
-		A list to store the elements of the stack.
+    Attributes
+    ----------
+    stack : list
+        A list to store the elements of the stack.
 
-	Methods
-	-------
-	__init__(self, a_list: list)
-		Initializes the stack with a given list.
+    Methods
+    -------
+    __init__(self, a_list: list)
+        Initializes the stack with a given list.
 
-	push(self, item: any)
-		Adds an element to the top of the stack.
+    push(self, item: any)
+        Adds an element to the top of the stack.
 
-	pop(self)
-		Removes the top element from the stack.
+    pop(self)
+        Removes the top element from the stack.
 
-	peek(self)
-		Prints the top element of the stack without removing it.
+    peek(self)
+        Prints the top element of the stack without removing it.
 
-	size(self)
-		Returns the number of elements in the stack.
+    size(self)
+        Returns the number of elements in the stack.
 
-	__repr__(self)
-		Returns a string representation of the stack.
-	"""
-	
-	def __init__(self, a_list: list):
-		"""
-		Initializes the stack with a given list.
+    __repr__(self)
+        Returns a string representation of the stack.
+    """
+    
+    def __init__(self, a_list: list):
+        """
+        Initializes the stack with a given list.
 
-		Parameters
-		----------
-		a_list : list
-			A list of elements to initialize the stack with.
-		"""
-		self.stack = a_list
-	
-	def push(self, item: any):
-		"""
-		Adds an element to the top of the stack.
+        Parameters
+        ----------
+        a_list : list
+            A list of elements to initialize the stack with.
+        """
+        self.stack = a_list
+    
+    def push(self, item: any):
+        """
+        Adds an element to the top of the stack.
 
-		Parameters
-		----------
-		item : any
-			The element to be added to the stack.
-		"""
-		self.stack.append(item)
-	
-	def pop(self):
-		"""
-		Removes the top element from the stack.
-		"""
-		self.stack.pop()
-	
-	def peek(self):
-		"""
-		Prints the top element of the stack without removing it.
-		"""
-		print(self.stack[-1])
-	
-	def size(self):
-		"""
-		Returns the number of elements in the stack.
+        Parameters
+        ----------
+        item : any
+            The element to be added to the stack.
+        """
+        self.stack.append(item)
+    
+    def pop(self):
+        """
+        Removes the top element from the stack.
+        """
+        self.stack.pop()
+    
+    def peek(self):
+        """
+        Prints the top element of the stack without removing it.
+        """
+        print(self.stack[-1])
+    
+    def size(self):
+        """
+        Returns the number of elements in the stack.
 
-		Returns
-		-------
-		int
-			The number of elements in the stack.
-		"""
-		return len(self.stack)
-	
-	def __repr__(self):
-		"""
-		Returns a string representation of the stack.
+        Returns
+        -------
+        int
+            The number of elements in the stack.
+        """
+        return len(self.stack)
+    
+    def __repr__(self):
+        """
+        Returns a string representation of the stack.
 
-		Returns
-		-------
-		str
-			A string representation of the stack.
-		"""
-		return repr(self.stack)
+        Returns
+        -------
+        str
+            A string representation of the stack.
+        """
+        return repr(self.stack)
 
 
 def ten(s: str) -> str:
-	"""
-	This function reverses a given string using recursion.
+    """
+    This function reverses a given string using recursion.
 
-	Parameters:
-	s (str): The string to be reversed.
+    Parameters:
+    s (str): The string to be reversed.
 
-	Returns:
-	str: A string that is the reverse of the input string. The returned string includes a brief description of the function's purpose, the reversed string
-	using Python's built-in reversed function, and the reversed string using recursion.
-	"""
-	ten = "reverse string using recursion"
-	
-	def reverse_string_with_recursion(s: str, t=None) -> str:
-		"""
-		A helper function that reverses a string using recursion.
+    Returns:
+    str: A string that is the reverse of the input string. The returned string includes a brief description of the function's purpose, the reversed string
+    using Python's built-in reversed function, and the reversed string using recursion.
+    """
+    ten = "reverse string using recursion"
+    
+    def reverse_string_with_recursion(s: str, t=None) -> str:
+        """
+        A helper function that reverses a string using recursion.
 
-		Parameters:
-		s (str): The string to be reversed.
-		t (str, optional): An accumulator string used to build the reversed string. Defaults to None.
+        Parameters:
+        s (str): The string to be reversed.
+        t (str, optional): An accumulator string used to build the reversed string. Defaults to None.
 
-		Returns:
-		str: The reversed string.
-		"""
-		if not t:
-			t = ''
-		t += list(s).pop() if len(list(s)) >= 1 else ''
-		return reverse_string_with_recursion(s[:-1], t=t) if list(s) else t
-	
-	run_it = reverse_string_with_recursion(s=s, t=None)
-	return f"{ten}\nthe str reversed: {''.join(list(reversed(s)))}\nRev str recursed: {run_it}\n{'*' * 72}"
+        Returns:
+        str: The reversed string.
+        """
+        if not t:
+            t = ''
+        t += list(s).pop() if len(list(s)) >= 1 else ''
+        return reverse_string_with_recursion(s[:-1], t=t) if list(s) else t
+    
+    run_it = reverse_string_with_recursion(s=s, t=None)
+    return f"{ten}\nthe str reversed: {''.join(list(reversed(s)))}\nRev str recursed: {run_it}\n{'*' * 72}"
 
 
 def eleven_(n: int) -> str:
-	"""
-	Computes the first n Fibonacci numbers.
+    """
+    Computes the first n Fibonacci numbers.
 
-	Parameters:
-	----------
-	n : int
-		The number of Fibonacci numbers to compute. Must be a positive integer.
+    Parameters:
+    ----------
+    n : int
+        The number of Fibonacci numbers to compute. Must be a positive integer.
 
-	Returns:
-	-------
-	str
-		A string containing the first n Fibonacci numbers. The string is formatted with a brief description of the function's purpose,
-		the computed Fibonacci numbers, and a horizontal rule for readability.
-	"""
-	eleven = "compute the first n fibonacci numbers"
-	
-	def fibonacci(n: int = None, n_list: list = None, og: int = None) -> list:
-		"""
-		A helper function to compute the Fibonacci numbers.
+    Returns:
+    -------
+    str
+        A string containing the first n Fibonacci numbers. The string is formatted with a brief description of the function's purpose,
+        the computed Fibonacci numbers, and a horizontal rule for readability.
+    """
+    eleven = "compute the first n fibonacci numbers"
+    
+    def fibonacci(n: int = None, n_list: list = None, og: int = None) -> list:
+        """
+        A helper function to compute the Fibonacci numbers.
 
-		Parameters:
-		----------
-		n : int
-			The number of Fibonacci numbers to compute. Must be a positive integer.
-		n_list : list
-			A list to store the computed Fibonacci numbers. Defaults to [0, 1].
-		og : int
-			The original value of n passed to the main function. Used to determine when to stop recursion.
+        Parameters:
+        ----------
+        n : int
+            The number of Fibonacci numbers to compute. Must be a positive integer.
+        n_list : list
+            A list to store the computed Fibonacci numbers. Defaults to [0, 1].
+        og : int
+            The original value of n passed to the main function. Used to determine when to stop recursion.
 
-		Returns:
-		-------
-		list
-			A list containing the first n Fibonacci numbers.
-		"""
-		if not n_list:
-			n_list = [0, 1]
-		da_sum = sum(n_list[-2:])
-		n_list.append(da_sum)
-		return fibonacci(n - 1, n_list, og=og) if len(n_list) < og else n_list
-	
-	out = fibonacci(n=n, n_list=None, og=10)
-	return f"{eleven}\nFirst {n} Fibonacci numbers: {out}\n{'*' * 72}"
+        Returns:
+        -------
+        list
+            A list containing the first n Fibonacci numbers.
+        """
+        if not n_list:
+            n_list = [0, 1]
+        da_sum = sum(n_list[-2:])
+        n_list.append(da_sum)
+        return fibonacci(n - 1, n_list, og=og) if len(n_list) < og else n_list
+    
+    out = fibonacci(n=n, n_list=None, og=10)
+    return f"{eleven}\nFirst {n} Fibonacci numbers: {out}\n{'*' * 72}"
 
 
 def generate_random_list(n, min_val, max_val):
-	the_list = [random.randint(min_val, max_val) for _ in range(n)]
-	the_list.append(the_list[0])  # to create a duplicate number in the list
-	return [random.randint(min_val, max_val) for _ in range(n)]
+    the_list = [random.randint(min_val, max_val) for _ in range(n)]
+    the_list.append(the_list[0])  # to create a duplicate number in the list
+    return [random.randint(min_val, max_val) for _ in range(n)]
 
 
 int_list = generate_random_list(100, 1, 100)
+
 # ############################################################################################################
 # def one(the_list: list, x: int) -> str:
 # 	ones = "check if integer x is in a list"
@@ -312,36 +314,41 @@ int_list = generate_random_list(100, 1, 100)
 # 	return f"{nine}\nMax: { max(int_list)}, Min: {min(int_list)}\n{'*' * 72}"
 # print(nine(int_list))
 ##################################################################################################
-##################################################################################################
 # print(ten(s="".join(str(i) for i in sorted(set(int_list))[:20])))
-##################################################################################################
-#
-
-
 #############################################################################################
 # print(eleven_(n=10))
-#############################################################################################
-twelve = 'sort list with quicksort algorithm'
-the_list_unsort = [9, 30, 93, 88, 85, 86, 90, 97, 78, 8]
-sorted_ = [8, 9, 30, 78, 85, 86, 88, 90, 93, 97]
+##########################################################################################
+# def twelve_(the_list: list) -> str:
+# 	twelve = 'sort list with quicksort algorithm'
+#
+# 	def quick_sort(a_list: list = None):
+# 		if a_list:
+# 			pivot = a_list.pop()
+# 			greater = [x for x in a_list if x > pivot]
+# 			lesser = [x for x in a_list if x <= pivot]
+# 			return quick_sort(lesser) + [pivot] + quick_sort(greater)
+# 		else:
+# 			return []
+#
+# 	my_list = quick_sort(the_list)
+# 	return f"{twelve}\nSorted list: {my_list}\n{'*' * 72}"
+# print(
+#     twelve_(the_list=int_list))
+##########################################################################################
+# thirteen = "for all possible permutations of a string"
+thirteen = 'nan'
+answer = {'nan', 'nna', 'ann'}
+
+set_thirteen = list(enumerate(i for i in thirteen if i != ' '))
+len_thirteen = len(set_thirteen)
+num_perm = math.prod([i for i in range(2, len_thirteen + 1)]) / math.prod([i for i in range(2, len_thirteen + 1)])
+
+for i in range():
+    pass
 
 
-def quick_sort(a_list: list = None):
-	start = a_list[random.randint(0, len(a_list) - 1)]
-	print(f"{start=}")
-	front_half = a_list[:a_list.index(start)]
-	back_half = a_list[a_list.index(start) + 1:]
-	a_list.remove(start)
-	new_list = [start]
-	for i in a_list:
-		if i < start:
-			new_list.insert(0, i)
-		else:
-			new_list.append(i)
-	
-	return quick_sort(new_list)
 
-
-my_list = quick_sort(the_list_unsort)
-print(f"{my_list=}")
-print(f"{sorted_=}")
+out = []
+for idx, let in set_thirteen:
+    print(f"{idx=}, {let=}")
+    
