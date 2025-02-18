af9b41e789489c643ed4ac85f2cd736f834c3ef3
jshensley <seizure_recognition@icloud.com>
2025-02-18 18:11:48 -0500

    dfy look see

--- DFY_Analysis/dfy_look_sees.py
+++ DFY_Analysis/dfy_look_sees.py
@@ -6,8 +6,6 @@ file_appt = '/Users/jshensley/Desktop/codewars/DFY_Analysis/appointment-reportin
 file_attr = '/Users/jshensley/Desktop/codewars/DFY_Analysis/attribution-reporting-2025-02-04_17-31-24.csv'
 file_call = '/Users/jshensley/Desktop/codewars/DFY_Analysis/call-reporting-2025-02-04_17-29-59.csv'
 
-
-
 df_call = pd.read_csv(file_call)
 df_call.astype(str)
 df_call = df_call.apply(lambda x: x.astype(str).str.strip().replace('-', np.nan))
@@ -21,8 +19,4 @@ unique_marketing_campaign = df_call['Marketing Campaign'].unique()
 unique_call_status = df_call['Call Status'].unique()
 
 df_appt = pd.read_csv(file_appt)
-
-
-
-
 df_attr = pd.read_csv(file_attr)
