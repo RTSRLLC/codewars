a5084403975975f4df06a27c14403d50cfb5bd2f
jshensley <seizure_recognition@icloud.com>
2025-10-26 11:24:00 -0400

    new challenge

--- solving_real_word_problems/time_savings_increasing_speeds.py
+++ solving_real_word_problems/time_savings_increasing_speeds.py
@@ -4,6 +4,14 @@
 import pandas as pd
 import numpy as np
 
+
+def lag_roll(df: pd.DataFrame, hr: str, time_frame: str):
+    for i in range(1, len(df_)):
+        col = f'{time_frame}_w/{i}_roll'
+        df_.loc[:, col] = hr.rolling(i).sum().round(4)
+    return df
+
+
 speeds = [35, 40, 45, 50, 55, 60, 65, 70, 75, 80]
 
 short_distances = [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 70, 80, 90, 100, 110, 120, 150, 200, 250, 300, 350, 400, 500, 600, 700, 800]
@@ -29,14 +37,19 @@ df = pd.DataFrame(arr, columns=cols)
 df_final = None
 set_it = df.distance.unique()
 for d in set_it:
-    df_ = df[df.distance == d]
-    df_['hr_diff'] = round(df_['hrs'].diff(), 4)
+    df_ = df[df.distance == d].copy()
 
-    df_['mins'] = round(df_['hrs'] * 60, 4)
-    df_['min_diff'] = round(df_['mins'].diff(), 4)
+    hrs = df_['hrs']
 
-    df_['secs'] = round(df_['hrs'] * 3600, 4)
-    df_['secs_diff'] = round(df_['secs'].diff(), 4)
+    df_ = lag_roll(df_, hrs, 'hrs')
+
+    df_.loc[:, 'mins'] = (hrs * 60).round(4)
+
+    df_ = lag_roll(df_, hrs, 'mins')
+
+    df_.loc[:, 'secs'] = (hrs * 3600).round(4)
+
+    df_ = lag_roll(df_, hrs, 'secs')
 
     df_ = df_.fillna(0)
 
