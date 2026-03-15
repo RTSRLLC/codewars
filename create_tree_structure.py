658bda7a2b0e4baa3593f4a1e019460b8c8b1f0f
jshensley <seizure_recognition@icloud.com>
2026-03-15 06:59:57 -0400

    working on creating a tree

--- create_tree_structure.py
+++ create_tree_structure.py
@@ -5,16 +5,39 @@ import warnings
 
 from pandas.core.config_init import performance_warnings
 
+# 1 open df
 file = '/Users/jshensley/Desktop/PycharmProjects/codewars/input_files/create_tree_python.xlsx'
 df = pd.read_excel(file)
+
+# 2 create dup columns
 df.insert(2, 'date_dup', df.plaedt == df.psdlm)
 
-# todo: goal: create a dataframe with unique combinations of the columns values
-cols = ['plaedt', 'psdlm', 'date_dup', 'plpln', 'plpln_dup', 'sku', 'sku_dup','pscrtn', 'pscrtn_dup']
-un1_date = df.date_dup.unique().tolist()
-un2_plpln = df.plpln_dup.unique().tolist()
-un3_sku_dup = df.sku_dup.unique().tolist()
-un4_pscrtn_dup = df.pscrtn_dup.unique().tolist()
+# id dup cols
+cols = [i for i in df.columns if '_dup' in i]
+
+# get unique vals in col
+nodes = []  # [(1, [False]), (2, [False, True]), (3, [False, True]), (4, [False, True])]
+num = 1
+for col in cols:
+    d = df[col].unique().tolist()
+    nodes.append((str(num), d, len(d)))
+    num += 1
+
+# determine number of possibilities but I don't think it matters if first node is 1 or 2 in length
+# formula: if there is a single val in parent node,
+# number of children nodes is:
+# (number_cols ^ 2) /2
+# if there is a binary parent node:
+# then number_cols ^ 2
+length = [i[2] for i in nodes]
+tree_formula = (len(length) ** 2) / 2 if length[0] == 1 else len(length) ** 2
+
+# tree: [('1', [False], 1), ('2', [False, True], 2), ('3', [False, True], 2), ('4', [False, True], 2)]
+for node in nodes:
+
+    stop = ''
+
+# so number of leaves = 8
 
 d = {
     1: [False, False, False, False],
@@ -33,10 +56,9 @@ tree = {}
 start = False
 length = 4
 
+single_start = [(1, False), (2, [True, False]), (3, [True, False]), (4, [True, False])]
+single_dict = {}
 
+multi_start = [(1, [True, False]), (2, [True, False]), (3, [True, False]), (4, [True, False])]
+multi_dict = {}
 # keep track of index
-
-
-
-
-# todo: goal2: print out the structure like a tree in the console
