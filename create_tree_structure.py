44988118b13d2de8b55c4fc773417e0cdc7b2564
jshensley <seizure_recognition@icloud.com>
2026-03-23 09:33:03 -0400

    working on creating a tree

--- create_tree_structure.py
+++ create_tree_structure.py
@@ -1,7 +1,10 @@
+from multiprocessing.connection import default_family
+
 import pandas as pd
 import numpy as np
 from itertools import combinations, permutations
 import warnings
+from dataclasses import dataclass, field
 
 from pandas.core.config_init import performance_warnings
 
@@ -9,35 +12,48 @@ from pandas.core.config_init import performance_warnings
 file = '/Users/jshensley/Desktop/PycharmProjects/codewars/input_files/create_tree_python.xlsx'
 df = pd.read_excel(file)
 
-# 2 create dup columns
+# 2 create date dup column...there are already other dup cols from source
 df.insert(2, 'date_dup', df.plaedt == df.psdlm)
 
-# id dup cols
+# id dup cols  ... must be in order ... i believe they will be since it iterates left to right
 cols = [i for i in df.columns if '_dup' in i]
 
-# get unique vals in col
-nodes = []  # [(1, [False]), (2, [False, True]), (3, [False, True]), (4, [False, True])]
-num = 1
+# get unique vals in dup cols for class Node
+nodes = {}  # [(1, [False]), (2, [False, True]), (3, [False, True]), (4, [False, True])]
+num = 0
 for col in cols:
     d = df[col].unique().tolist()
-    nodes.append((str(num), d, len(d)))
+    nodes[num] = {"node_val": d, "num_values": len(d)}
     num += 1
 
-# determine number of possibilities but I don't think it matters if first node is 1 or 2 in length
-# formula: if there is a single val in parent node,
-# number of children nodes is:
-# (number_cols ^ 2) /2
-# if there is a binary parent node:
-# then number_cols ^ 2
-length = [i[2] for i in nodes]
-tree_formula = (len(length) ** 2) / 2 if length[0] == 1 else len(length) ** 2
 
-# tree: [('1', [False], 1), ('2', [False, True], 2), ('3', [False, True], 2), ('4', [False, True], 2)]
+@dataclass
+class Node:
+    """
+    A bool class representing a node in the tree structure.
+    Args:
+        node_val: boolean, True or False
+        node_address: int, first digit = tree level, 0 being parent, additional digits = number
+        of lines from left margin
+        parent: Node, the parent of this node
+        children: dict, a dictionary that maps child nodes to their children using node_address
+
+    """
+    node_val: bool
+    node_address: int
+    parent_address: int
+    children_addresses: dict = field(default_factory=dict)  # {"left": None, "right": None}
+
+
+# {0: {'Node': [False], 'num_values': 1}, 1: {'Node': [False, True], 'num_values': 2},
+# 2: {'Node': [False, True], 'num_values': 2}, 3: {'Node': [False, True], 'num_values': 2}}
+tree = {}
 for node in nodes:
-
+    curr_node = nodes.get(node)
+    val = curr_node.get('node_val')
     stop = ''
 
-# so number of leaves = 8
+# number of leaves = 8
 
 d = {
     1: [False, False, False, False],
@@ -51,14 +67,3 @@ d = {
     7: [False, True, True, False],
     8: [False, True, True, True],
     }
-
-tree = {}
-start = False
-length = 4
-
-single_start = [(1, False), (2, [True, False]), (3, [True, False]), (4, [True, False])]
-single_dict = {}
-
-multi_start = [(1, [True, False]), (2, [True, False]), (3, [True, False]), (4, [True, False])]
-multi_dict = {}
-# keep track of index
